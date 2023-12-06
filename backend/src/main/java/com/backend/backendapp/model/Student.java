12c58131e55c9cb5d6aefee52d23a568408e641d
SUBHAM BANERJEE <subhambanerjee2003@gmail.com>
2023-12-06 13:39:31 +0530

    Enabled Lombok in vsc and eliminated redundantcy

--- backend/src/main/java/com/backend/backendapp/model/Student.java
+++ backend/src/main/java/com/backend/backendapp/model/Student.java
@@ -1,5 +1,7 @@
 package com.backend.backendapp.model;
 
+import org.hibernate.annotations.NaturalId;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,7 +10,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.NaturalId;
 
 @Entity
 @Setter
@@ -20,43 +21,12 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     private String firstName;
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 
     private String lastName;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     @NaturalId(mutable = true)
     private String email;
+
     private String department;
 }
